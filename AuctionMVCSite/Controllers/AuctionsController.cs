7bb569ef86f451e87a644654229300859e0883e4
Mohammed Alom <mohammed_alom@mcafee.com>
2019-03-18 18:21:36 +0000

    -First commit to the git - its a basic Auction website

---
+++ AuctionMVCSite/Controllers/AuctionsController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuctionMVCSite.Controllers
+{
+    public class AuctionsController : Controller
+    {
+        // GET: Auctions
+        public ActionResult Index()
+        {
+            var auctions = new[]{
+                new Models.Auction()
+                {
+                    Title = "Example Auction #1",
+                    Description = "This is an example Auction",
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now.AddDays(7),
+                    StartPrice = 1.00m,
+                    CurrentPrice = null,
+                },
+                new Models.Auction()
+                {
+                    Title = "Example Auction #2",
+                    Description = "This is an example Auction",
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now.AddDays(7),
+                    StartPrice = 1.00m,
+                    CurrentPrice = null,
+                },
+                new Models.Auction()
+                {
+                    Title = "Example Auction #3",
+                    Description = "This is an example Auction",
+                    StartTime = DateTime.Now,
+                    EndTime = DateTime.Now.AddDays(7),
+                    StartPrice = 1.00m,
+                    CurrentPrice = null,
+                },
+            };
+            return View(auctions);
+        }
+
+        public ActionResult TempDataDemo()
+        {
+            TempData["SuccessMessage"] = "The action succeeded";
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Auction()
+        {
+            var auction = new AuctionMVCSite.Models.Auction()
+            {
+                Title = "Example Auction",
+                Description = "This is an example Auction",
+                StartTime = DateTime.Now,
+                EndTime = DateTime.Now.AddDays(7),
+                StartPrice = 1.00m,
+                CurrentPrice = null,
+            };
+            //view data dictionary 
+            //ViewData["Auction"] = auction;
+            //return View();
+            //I can use to render view like this
+            return View(auction);
+        }
+    }
+}
---
+++ AuctionMVCSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AuctionMVCSite.Startup))]
+namespace AuctionMVCSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
