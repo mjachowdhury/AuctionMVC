f70580d1ce9f6aea89c060e9aa4d53bbf5615a67
Mohammed Alom <mohammed_alom@mcafee.com>
2019-07-12 19:41:30 +0100

    How to validated the user input data added

--- AuctionMVCSite/Controllers/AuctionsController.cs
+++ AuctionMVCSite/Controllers/AuctionsController.cs
@@ -67,12 +67,41 @@ namespace AuctionMVCSite.Controllers
             return View(auction);
         }
 
-        //here [Bind(Exclude ="CurrentPrice")] will ignore the current price from the post so the use ca not see the current price bid
-        public ActionResult Create([Bind(Exclude ="CurrentPrice")] Models.Auction auction)
+        //here two actionresult create method one wil ldo get and other will do post 
+        //other wise asp.net will get confuse as it will be called tewo time and which oine to call fist will get confuse then will show error result
+
+        [HttpGet]
+        public ActionResult Create()
         {
             var categoryList = new SelectList(new[] { "Automotive", "Electronics", "Games", "Homes" });
             ViewBag.CategoryList = categoryList;
             return View();
+        }
+
+        //here [Bind(Exclude ="CurrentPrice")] will ignore the current price from the post so the use ca not see the current price bid
+
+        [HttpPost]
+        public ActionResult Create([Bind(Exclude ="CurrentPrice")] Models.Auction auction)
+        {
+            /*
+            if (string.IsNullOrWhiteSpace(auction.Title))
+            {
+                ModelState.AddModelError("Title", "Title is required");
+            }
+            else if(auction.Title.Length < 5 || auction.Title.Length > 200)
+            {
+                ModelState.AddModelError("Title", "Title must be between 5 and 200 characters long");
+            }
+            */
+            if (ModelState.IsValid)
+            {
+                //save to the databse
+                return RedirectToAction("Index");
+            }
+
+            return Create();
+
+          
         }
     }
 }
